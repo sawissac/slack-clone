ac085791be65d2ea6ece09f86b80ebc774a606b3
sawissac <issacmm64@gmail.com>
2024-07-12 14:49:31 +0700

    fix deploy bugs

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,4 +1,11 @@
-import { parseEmail } from "../lib";
+function parseEmail(email) {
+  if (!email) return "";
+  let parsedAtSign = email.split("@")?.at(0);
+  let parsePlusSign = parsedAtSign?.split("+");
+  let parsedPlusSign = parsePlusSign?.at(0);
+  return parsedPlusSign;
+}
+
 
 describe("checking lib functions", () => {
   it("email contain with abc@gmail.com convert to abc", () => {
